d17b343c2901b588dbc0e88416de5de3684df57f
TGIfr <mihail.lukjanec@gmail.com>
2020-12-18 01:41:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinesCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
